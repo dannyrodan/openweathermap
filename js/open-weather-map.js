5f643a5a0078dfea1241f0bd3ed4fe704651a871
Vell <vmcilwain@me.com>
2015-12-01 19:58:54 -0500

    add bootstrap, angular and jquery. Start building out template and controllers

---
+++ js/open-weather-map.js
@@ -0,0 +1,20 @@
+(function(){
+  var app = angular.module('open-weather', []);
+  
+  app.controller('city1Controller', function(){
+    this.weatherData = weatherData;
+  });
+  
+  app.controller('city2Controller', function(){
+    this.weatherData = weatherData;
+  });
+  
+  app.controller('city3Controller', function(){
+    this.weatherData = weatherData;
+  });
+})()
+
+var weatherData = {
+  name: 'San Fran Florida',
+  temperature: '70 degress'
+};
